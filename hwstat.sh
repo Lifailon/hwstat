b413a86b9586398d7267a295343929566bee9dc7
Alex Kup <116945542+Lifailon@users.noreply.github.com>
2023-08-27 13:30:22 +0300

    version: 0.2

--- hwstat.sh
+++ hwstat.sh
@@ -1,6 +1,4 @@
 function hwstat {
-# apt list --installed
-# cat /etc/passwd
 hn=$(uname -a | awk '{print $2}')
 time=$(timedatectl | grep Local | sed -E "s/.+time: //")
 tz=$(timedatectl | grep zone | sed -E "s/.+zone: //")
@@ -14,53 +12,76 @@ core=$(lscpu | grep "^CPU(s)" | awk '{print$2}')
 arch=$(lscpu | grep "Architecture" | sed -E "s/Architecture:\s+//")
 l2=$(lscpu | grep L2 | awk '{print $3,$4}')
 l3=$(lscpu | grep L3 | awk '{print $3,$4}')
-mem=$(free -m | grep Mem | awk '{print $2}')
-swap=$(free -m | grep Swap | awk '{print $2}')
-mount=$(swapon | sed '1d' | awk '{print $2,$1}')
 mb=$(dmidecode -t baseboard | grep Product | awk -F": " '{print $2}')
 bios=$(dmidecode -t bios | grep Vendor | sed -E "s/.+Vendor: //")
 ver=$(dmidecode -t bios | grep Version | sed -E "s/.+Version: //")
 rel=$(dmidecode -t bios | grep Release | sed -E "s/.+Release Date: //" | sed "s/\//./g")
+mem=$(free -m | grep Mem | awk '{print $2}')
+swap=$(free -m | grep Swap | awk '{print $2}')
+mount=$(swapon | sed '1d' | awk '{print $2,$1}')
 eth=$(lspci | grep -i ethernet | awk -F": " '{print $NF}')
 video=$(lspci | grep -i vga | awk -F": " '{print $NF}')
 audio=$(lspci | grep -i audio | awk -F": " '{print $NF}')
 scsi=$(lspci | grep -i scsi | awk -F": " '{print $NF}')
 sata=$(lspci | grep -i sata | awk -F": " '{print $NF}')
-map=$(mount | grep "mapper")
 sd=$(ls -l /dev | grep sd | awk '{print $NF}')
 sd=$(echo $sd | sed -E "s/\s/, /g")
+lsblk=$(lsblk | grep -w "sd.." | awk '{print $1" ("$4"),"}' | sed -r "s/├─|└─//")
+lsblk=$(echo $lsblk | sed -r "s/,$//")
+pvs=$(pvs | sed "1d; s/<//" | awk '{print $1" -> "$2" ("$6"/"$5")"}')
+vgs=$(vgs | sed "1d; s/<//" | awk '{print $1" pdisk:"$2" lgroup:"$3" ("$7"/"$6")"}')
+lvs=$(lvs | sed "1d; s/<//" | awk '{print $1" "$2" ("$4")"}')
+usr=$(cat /etc/passwd | wc -l)
+home=($(ls /home))
+home=$(echo ${home[@]} | sed "s/ /, /")
 int=($(ls /sys/class/net))
 int=$(echo ${int[@]} | sed "s/ /, /")
 dns=$(resolvectl status | grep "Current DNS" | awk -F": " '{print $2}')
-echo "Hostname            : $hn"
-echo "Local Time          : $time"
-echo "Time Zone           : $tz"
-echo "NTP service         : $ntp"
-echo "NTP server          : $srv"
-echo "Kernel              : $kernel"
-echo "OS                  : $os"
-echo "Hypervisor          : $vm"
-echo "CPU                 : $cpu"
-echo "Core                : $core"
-echo "Architecture        : $arch"
-echo "L2                  : $l2"
-echo "L3                  : $l3"
-echo "Memory              : $mem MB"
-echo "SWAP                : $swap MB"
-echo "SWAP Mount          : $mount"
-echo "Motherboard         : $mb"
-echo "BIOS                : $bios"
-echo "Version             : $ver"
-echo "Release             : $rel"
-echo "Ethernet Adapter    : $eth "
-echo "VGA controller      : $video"
-echo "Audio controller    : $audio"
-echo "SCSI controller     : $scsi"
-echo "SATA controller     : $sata"
-echo "Mount fs            : $map"
-echo "Disk                : $sd"
-echo "Network Interfaces  : $int"
-echo "Current DNS Server  : $dns"
+dpkg=$(dpkg -l | wc -l)
+showauto=$(apt-mark showauto | wc -l)
+showmanual=$(apt-mark showmanual | wc -l)
+zabbix_status=$(systemctl status zabbix-agent | grep Active | awk -F": " '{print $2}')
+zabbix_path=$(systemctl status zabbix-agent | grep -P -o "(?<=-c ).*(?=.conf)" | sed "s/$/.conf/")
+zabbix_server=$(cat $zabbix_path | grep -Po "(?<=^Server=).+")
+echo "Hostname              : $hn"
+echo "Local Time            : $time"
+echo "Time Zone             : $tz"
+echo "NTP service           : $ntp"
+echo "NTP server            : $srv"
+echo "Kernel                : $kernel"
+echo "OS                    : $os"
+echo "Hypervisor            : $vm"
+echo "CPU                   : $cpu"
+echo "Core                  : $core"
+echo "Architecture          : $arch"
+echo "L2                    : $l2"
+echo "L3                    : $l3"
+echo "Motherboard           : $mb"
+echo "BIOS                  : $bios"
+echo "BIOS Version          : $ver"
+echo "BIOS Release          : $rel"
+echo "Memory                : $mem MB"
+echo "SWAP                  : $swap MB"
+echo "SWAP Mount            : $mount"
+echo "Ethernet Adapter      : $eth "
+echo "VGA controller        : $video"
+echo "Audio controller      : $audio"
+echo "SCSI controller       : $scsi"
+echo "SATA controller       : $sata"
+echo "Disk and Volume       : $sd"
+echo "Disk and Volume size  : $lsblk"
+echo "Physical Volume       : $pvs"
+echo "Volume Group          : $vgs"
+echo "Logical Volume        : $lvs"
+echo "User count            : $usr"
+echo "User directories      : $home"
+echo "Network Interfaces    : $int"
+echo "Current DNS Server    : $dns"
+echo "DPKG Packet count     : $dpkg"
+echo "APT show auto/manual  : $showauto/$showmanual"
+echo "Zabbix agent status   : $zabbix_status"
+echo "Zabbix config         : $zabbix_path"
+echo "Zabbix server         : $zabbix_server"
 }
 
 hwstat
