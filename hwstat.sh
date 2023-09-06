03963b2d0219e021e779bed1342e22c3f74052b3
Alex Kup <116945542+Lifailon@users.noreply.github.com>
2023-09-06 14:14:03 +0300

    version: 0.5

--- hwstat.sh
+++ hwstat.sh
@@ -2,6 +2,7 @@ function hwstat {
 hn=$(uname -a | awk '{print $2}')
 uptime=$(uptime | sed -E "s/^ ..:..:.. up //; s/ [0-9] user.+//; s/,//g")
 startup=$(systemd-analyze | sed -n "1p" | sed "s/Startup finished in //")
+systemd_ver=$(systemd --version | grep -Eo "\(.+\)" | sed -E "s/\(|\)//g")
 time=$(timedatectl | grep Local | sed -E "s/.+time: //")
 tz=$(timedatectl | grep zone | sed -E "s/.+zone: //")
 ntp=$(timedatectl | grep NTP | sed -E "s/.+service: //")
@@ -18,8 +19,28 @@ mb=$(dmidecode -t baseboard | grep Product | awk -F": " '{print $2}')
 bios=$(dmidecode -t bios | grep Vendor | sed -E "s/.+Vendor: //")
 ver=$(dmidecode -t bios | grep Version | sed -E "s/.+Version: //")
 rel=$(dmidecode -t bios | grep Release | sed -E "s/.+Release Date: //" | sed "s/\//./g")
-mem=$(free -m | grep Mem | awk '{print $2}')
-swap=$(free -m | grep Swap | awk '{print $2}')
+ps=$(ps -A | sed 1d | wc -l)
+run=$(cat /proc/loadavg | awk '{print $4}')
+avg=$(cat /proc/loadavg | awk '{print $1,$2,$3}')
+vmstat=$(vmstat 1 2)
+vms_avg=$(printf "%s\n" "${vmstat[@]}" | sed -n 3p)
+vms_cur=$(printf "%s\n" "${vmstat[@]}" | sed -n 4p)
+us_avg=$(echo $vms_avg | awk '{print $13}')
+us_cur=$(echo $vms_cur | awk '{print $13}')
+sy_avg=$(echo $vms_avg | awk '{print $14}')
+sy_cur=$(echo $vms_cur | awk '{print $14}')
+wa_avg=$(echo $vms_avg | awk '{print $16}')
+wa_cur=$(echo $vms_cur | awk '{print $16}')
+id_avg=$(echo $vms_avg | awk '{print $15}')
+id_cur=$(echo $vms_cur | awk '{print $15}')
+bi_avg=$(echo $vms_avg | awk '{print $9}')
+bi_cur=$(echo $vms_cur | awk '{print $9}')
+bo_avg=$(echo $vms_avg | awk '{print $10}')
+bo_cur=$(echo $vms_cur | awk '{print $10}')
+mem_all=$(free -m | grep Mem | awk '{print $2}')
+mem_use=$(free -m | grep Mem | awk '{print $3}')
+swap_all=$(free -m | grep Swap | awk '{print $2}')
+swap_use=$(free -m | grep Swap | awk '{print $3}')
 mount=$(swapon | sed '1d' | awk '{print $2,$1}')
 swaprun=$(sysctl vm.swappiness | awk -F "= " '{print $2}')
 eth=$(lspci | grep -i ethernet | awk -F": " '{print $NF}')
@@ -59,7 +80,12 @@ route_ip_forward=$(if (( $(echo "$route == 1" | bc) )); then echo "true"; else e
 use=$(cat /proc/sys/fs/file-nr | awk '{print $1}')
 descriptor=$(sysctl fs.file-max | awk -F "= " '{print $2}')
 lsof=$(lsof | sed 1d | wc -l)
+du=$(du -a / 2> /dev/null | wc -l)
 limits=$(cat /etc/security/limits.conf | grep -Ev "^$|^#" | wc -l)
+ss=$(ss -tun | wc -l)
+ports=$(ss -tun | sed 1d | awk '{print $5}' | awk -F ":" '{print $2","}' | sort | uniq)
+ports=$(echo $ports | sed -r "s/,$//")
+iptables=$(echo $(( $(iptables -L | wc -l) - 8 )))
 unit_all=$(systemctl list-unit-files | sed "1d" | wc -l)
 unit_startup=$(systemctl list-unit-files --state=enabled | sed "1d" | wc -l)
 showauto=$(apt-mark showauto | wc -l)
@@ -72,9 +98,11 @@ home=$(echo ${home[@]} | sed "s/ /, /g")
 zabbix_status=$(systemctl status zabbix-agent | grep Active | awk -F": " '{print $2}')
 zabbix_path=$(systemctl status zabbix-agent | grep -P -o "(?<=-c ).*(?=.conf)" | sed "s/$/.conf/")
 zabbix_server=$(cat $zabbix_path | grep -Po "(?<=^Server=).+")
+echo
 echo "Hostname                : $hn"
 echo "Uptime                  : $uptime"
 echo "Startup                 : $startup"
+echo "Systemd version         : $systemd_ver"
 echo "Local Time              : $time"
 echo "Time Zone               : $tz"
 echo "NTP service             : $ntp"
@@ -91,8 +119,15 @@ echo "Motherboard             : $mb"
 echo "BIOS                    : $bios"
 echo "BIOS Version            : $ver"
 echo "BIOS Release            : $rel"
-echo "Memory                  : $mem MB"
-echo "SWAP                    : $swap MB"
+echo "Process Started count   : $ps"
+echo "Running/All Process     : $run"
+echo "CPU avg 1/5/15 min      : $avg"
+echo -e "CPU avg usr/sys/wa/idle : $us_avg\t$sy_avg\t$wa_avg\t$id_avg"
+echo -e "CPU cur usr/sys/wa/idle : $us_cur\t$sy_cur\t$wa_cur\t$id_cur"
+echo -e "IOps avg in/out         : $bi_avg\t$bo_avg"
+echo -e "IOps current in/out     : $bi_cur\t$bo_cur"
+echo "Memory use/all          : $mem_use/$mem_all MB"
+echo "SWAP use/all            : $swap_use/$swap_all MB"
 echo "SWAP Mount              : $mount"
 echo "SWAP Running free mem   : $swaprun%"
 echo "Ethernet Adapter        : $eth "
@@ -118,7 +153,11 @@ echo "ICMP ignore             : $icmp"
 echo "Route ip forward        : $route_ip_forward"
 echo "Descriptor File use/max : $use/$descriptor"
 echo "List Open Files count   : $lsof"
+echo "All dir and files count : $du"
 echo "Limits count            : $limits"
+echo "Socket ESTAB count      : $ss"
+echo "Unique open net ports   : $ports"
+echo "iptables rule count     : $iptables"
 echo "Unit Startup count      : $unit_startup/$unit_all"
 echo "APT show auto/manual    : $showauto/$showmanual"
 echo "DPKG Packet count       : $dpkg"
@@ -128,6 +167,7 @@ echo "User directories        : $home"
 echo "Zabbix agent status     : $zabbix_status"
 echo "Zabbix config           : $zabbix_path"
 echo "Zabbix server           : $zabbix_server"
+echo
 }
 
-hwstat
+hwstat 2> /dev/null
